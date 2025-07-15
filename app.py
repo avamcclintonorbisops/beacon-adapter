93fea6a9329da65de7a49e0ad61158125d720a66
avamcclintonorbisops <a.mcclinton@orbisops.com>
2025-07-15 17:49:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,20 +8,22 @@ def home():
 
 @app.route('/beacon', methods=['POST'])
 def handle_beacon():
-    try:
-        data = request.get_json(silent=True)
-        print("Payload:", data)
+    data = request.get_json(silent=True)
 
-        input1 = data.get("input1", [])
-        input2 = data.get("input2", {})
+    print("Payload:", data)
 
-        print("input1:", input1)
-        print("input2:", input2)
+    if not data:
+        print("Raw (non-JSON) request:", request.data)
+        return '', 200  # Avoid crashing on bad input
 
-        return '', 200
-    except Exception as e:
-        print("Error:", e)
-        return jsonify({"status": "error", "details": str(e)}), 400
+    input1 = data.get("input1", [])
+    input2 = data.get("input2", {})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    print("input1:", input1)
+    print("input2:", input2)
+
+    return '', 200  # Return success always
+
+@app.route('/beacons', methods=['GET'])
+def get_beacons():
+    return jsonify({"message": "Not storing data in this
